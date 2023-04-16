20057cb497f0b7ad78e85c2fac17e9cf2ead507d
脆皮丶 <i@tsuiqg.xyz>
2023-04-16 19:44:35 +0800

    add v0.1.8

--- index.js
+++ index.js
@@ -1,357 +1,54 @@
-const { isPackageExists } = require('local-pkg')
-
-const TS = isPackageExists('typescript')
-
-if (!TS) console.warn('[@cuiqg/eslint-config] TypeScript is not installed, fallback to JS only.')
-
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 2022,
-    ecmaFeatures: {
-      jsx: true
-    },
-    sourceType: 'module'
-  },
   env: {
     es6: true,
     browser: true,
     node: true,
-    es2021: true
   },
   globals: {
     document: 'readonly',
     navigator: 'readonly',
     window: 'readonly'
   },
-  reportUnusedDisableDirectives: true,
   extends: [
-    'plugin:vue/vue3-recommended',
+    "eslint:recommended",
     'plugin:import/recommended',
-    'plugin:eslint-comments/recommended',
-    'plugin:jsonc/recommended-with-jsonc',
-    'plugin:yml/standard',
-    'plugin:markdown/recommended',
-    'prettier'
+    'plugin:vue/vue3-recommended'
   ],
   ignorePatterns: [
-    '*.min.*',
-    '*.d.ts',
-    'CHANGELOG.md',
-    'dist',
-    'LICENSE*',
-    'output',
-    'out',
-    'coverage',
-    'public',
-    'temp',
-    'package-lock.json',
-    'pnpm-lock.yaml',
-    'yarn.lock',
-    '__snapshots__',
-    // ignore for in lint-staged
-    '*.css',
-    '*.png',
-    '*.ico',
-    '*.toml',
-    '*.patch',
-    '*.txt',
-    '*.crt',
-    '*.key',
-    'Dockerfile',
-    // force include
-    '!.github',
-    '!.vitepress',
-    '!.vscode'
+    "**/*.config.js"
   ],
-  plugins: ['html', 'unicorn', 'markdown', 'import', 'n', 'promise', 'unused-imports'],
+  "reportUnusedDisableDirectives": true,
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['html'],
   overrides: [
     {
-      files: ['*.json', '*.json5'],
-      parser: 'jsonc-eslint-parser',
-      rules: {
-        'jsonc/array-bracket-spacing': ['error', 'never'],
-        'jsonc/comma-dangle': ['error', 'never'],
-        'jsonc/comma-style': ['error', 'last'],
-        'jsonc/indent': ['error', 2],
-        'jsonc/key-spacing': ['error', { beforeColon: false, afterColon: true }],
-        'jsonc/no-octal-escape': 'error',
-        'jsonc/object-curly-newline': ['error', { multiline: true, consistent: true }],
-        'jsonc/object-curly-spacing': ['error', 'always'],
-        'jsonc/object-property-newline': ['error', { allowMultiplePropertiesPerLine: true }]
-      }
-    },
-    {
-      files: ['*.yaml', '*.yml'],
-      parser: 'yaml-eslint-parser',
-      rules: {
-        'spaced-comment': 'off'
-      }
-    },
-    {
-      files: ['package.json'],
-      parser: 'jsonc-eslint-parser',
-      rules: {
-        'jsonc/sort-keys': [
-          'error',
-          {
-            pathPattern: '^$',
-            order: [
-              'publisher',
-              'name',
-              'displayName',
-              'type',
-              'version',
-              'private',
-              'packageManager',
-              'description',
-              'author',
-              'license',
-              'funding',
-              'homepage',
-              'repository',
-              'bugs',
-              'keywords',
-              'categories',
-              'sideEffects',
-              'exports',
-              'main',
-              'module',
-              'unpkg',
-              'jsdelivr',
-              'types',
-              'typesVersions',
-              'bin',
-              'icon',
-              'files',
-              'engines',
-              'activationEvents',
-              'contributes',
-              'scripts',
-              'peerDependencies',
-              'peerDependenciesMeta',
-              'dependencies',
-              'optionalDependencies',
-              'devDependencies',
-              'pnpm',
-              'overrides',
-              'resolutions',
-              'husky',
-              'simple-git-hooks',
-              'lint-staged',
-              'eslintConfig'
-            ]
-          },
-          {
-            pathPattern: '^(?:dev|peer|optional|bundled)?[Dd]ependencies$',
-            order: { type: 'asc' }
-          },
-          {
-            pathPattern: '^exports.*$',
-            order: ['types', 'require', 'import']
-          }
-        ]
-      }
-    },
-    {
-      files: ['**/*.md'],
-      processor: 'markdown/markdown'
-    },
-    {
-      files: ['**/*.md/*.*'],
-      rules: {
-        'import/no-unresolved': 'off',
-        'unused-imports/no-unused-imports': 'off',
-        'unused-imports/no-unused-vars': 'off',
-        'no-alert': 'off',
-        'no-console': 'off',
-        'no-restricted-imports': 'off',
-        'no-undef': 'off',
-        'no-unused-expressions': 'off',
-        'no-unused-vars': 'off'
-      }
-    },
-    {
       files: ['*.vue'],
-      parser: 'vue-eslint-parser',
       rules: {
         'no-unused-vars': 'off',
         'no-undef': 'off',
-        'no-use-before-define': 'off',
-        indent: 'off'
+        'no-use-before-define': 'off'
       }
     }
   ],
   rules: {
-    // import
-    'import/order': 'error',
-    'import/first': 'error',
-    'import/no-mutable-exports': 'error',
-    'import/no-unresolved': 'off',
-    'import/no-absolute-path': 'off',
-    'import/newline-after-import': ['error', { count: 1, considerComments: true }],
-
-    // Common
-    semi: ['error', 'never'],
-    curly: ['error', 'all'],
-    quotes: ['error', 'single'],
-    'quote-props': ['error', 'consistent-as-needed'],
-
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': ['warn', { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }],
-
-    'no-param-reassign': 'off',
-    'array-bracket-spacing': ['error', 'never'],
-    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
-    'block-spacing': ['error', 'always'],
-    camelcase: 'off',
-    'comma-spacing': ['error', { before: false, after: true }],
-    'comma-style': ['error', 'last'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'no-constant-condition': 'warn',
-    'no-debugger': 'error',
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    'no-cond-assign': ['error', 'always'],
-    'func-call-spacing': ['off', 'never'],
-    'key-spacing': ['error', { beforeColon: false, afterColon: true }],
-    indent: ['error', 2, { SwitchCase: 1, VariableDeclarator: 1, outerIIFEBody: 1 }],
-    'no-restricted-syntax': ['error', 'DebuggerStatement', 'LabeledStatement', 'WithStatement'],
-    'object-curly-spacing': ['error', 'always'],
-    'no-return-await': 'off',
-    'space-before-function-paren': [
-      'error',
-      {
-        anonymous: 'always',
-        named: 'never',
-        asyncArrow: 'always'
-      }
+    "no-unused-vars": [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
     ],
-
-    // es6
-    'no-var': 'error',
-    'prefer-const': [
-      'error',
-      {
-        destructuring: 'all',
-        ignoreReadBeforeAssign: true
-      }
-    ],
-    'prefer-arrow-callback': [
-      'error',
-      {
-        allowNamedFunctions: false,
-        allowUnboundThis: true
-      }
-    ],
-    'object-shorthand': [
-      'error',
-      'always',
-      {
-        ignoreConstructors: false,
-        avoidQuotes: true
-      }
-    ],
-    'prefer-exponentiation-operator': 'error',
-    'prefer-rest-params': 'error',
-    'prefer-spread': 'error',
-    'prefer-template': 'error',
-    'template-curly-spacing': 'error',
-    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
-    'generator-star-spacing': 'off',
-    'spaced-comment': [
-      'error',
-      'always',
-      {
-        line: {
-          markers: ['/'],
-          exceptions: ['/', '#']
-        },
-        block: {
-          markers: ['!'],
-          exceptions: ['*'],
-          balanced: true
-        }
-      }
-    ],
-
-    // best-practice
-    'array-callback-return': 'error',
-    'block-scoped-var': 'error',
-    'consistent-return': 'off',
-    complexity: ['off', 11],
-    eqeqeq: ['error', 'smart'],
-    'no-alert': 'warn',
-    'no-case-declarations': 'error',
-    'no-multi-spaces': 'error',
-    'no-multi-str': 'error',
-    'no-with': 'error',
-    'no-void': 'error',
-    'no-useless-escape': 'off',
-    'vars-on-top': 'error',
-    'require-await': 'off',
-    'no-return-assign': 'off',
-    'operator-linebreak': ['error', 'before'],
-    'max-statements-per-line': ['error', { max: 1 }],
-
-    // node
-    // 'n/prefer-global/process': ['error', 'never'], // Not sure if we need it as we are using `process.env.NODE_ENV` a lot in front-end.
-    'n/prefer-global/buffer': ['error', 'never'],
-    'n/no-callback-literal': 'off',
-
-    // unicorns
-    // Pass error message when throwing errors
-    'unicorn/error-message': 'error',
-    // Uppercase regex escapes
-    'unicorn/escape-case': 'error',
-    // Array.isArray instead of instanceof
-    'unicorn/no-instanceof-array': 'error',
-    // Prevent deprecated `new Buffer()`
-    'unicorn/no-new-buffer': 'error',
-    // Keep regex literals safe!
-    'unicorn/no-unsafe-regex': 'off',
-    // Lowercase number formatting for octal, hex, binary (0x1'error' instead of 0X1'error')
-    'unicorn/number-literal-case': 'error',
-    // includes over indexOf when checking for existence
-    'unicorn/prefer-includes': 'error',
-    // String methods startsWith/endsWith instead of more complicated stuff
-    'unicorn/prefer-string-starts-ends-with': 'error',
-    // textContent instead of innerText
-    'unicorn/prefer-text-content': 'error',
-    // Enforce throwing type error when throwing error while checking typeof
-    'unicorn/prefer-type-error': 'error',
-    // Use new when throwing error
-    'unicorn/throw-new-error': 'error',
-    // Prefer using the node: protocol
-    'unicorn/prefer-node-protocol': 'error',
-
-    'no-use-before-define': ['error', { functions: false, classes: false, variables: true }],
-    'eslint-comments/disable-enable-pair': 'off',
-    'import/no-named-as-default-member': 'off',
-    'import/no-named-as-default': 'off',
-    'import/namespace': 'off',
-
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: false,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        allowSeparatedGroups: false
-      }
-    ],
-
-    // yml
-    'yml/quotes': ['error', { prefer: 'single', avoidEscape: false }],
-    'yml/no-empty-document': 'off',
-
-    // vue
-    'vue/max-attributes-per-line': 'off',
-    'vue/no-v-html': 'off',
-    'vue/require-prop-types': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/multi-word-component-names': 'off',
-    'vue/prefer-import-from-vue': 'off',
-    'vue/no-v-text-v-html-on-component': 'off',
+    "import/no-unresolved": "off",
+    "vue/no-v-html": "off",
+    "vue/valid-v-for": "off",
+    "vue/require-v-for-key": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/component-tags-order": ["error", {
+      "order": [["template", "script"], "style"]
+    }],
     'vue/script-indent': [
       'error',
       2,
@@ -361,83 +58,14 @@ module.exports = {
         ignores: []
       }
     ],
-
-    // reactivity transform
-    'vue/no-setup-props-destructure': 'off',
-
-    'vue/component-tags-order': [
-      'error',
-      {
-        order: ['template', 'script', 'style']
-      }
-    ],
-    'vue/block-tag-newline': [
-      'error',
-      {
-        singleline: 'always',
-        multiline: 'always'
-      }
-    ],
-    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-    'vue/component-options-name-casing': ['error', 'PascalCase'],
-    'vue/custom-event-name-casing': ['error', 'camelCase'],
-    'vue/define-macros-order': [
-      'error',
-      {
-        order: ['defineProps', 'defineEmits']
-      }
-    ],
-    'vue/html-comment-content-spacing': [
-      'error',
-      'always',
-      {
-        exceptions: ['-']
-      }
-    ],
-    'vue/no-restricted-v-bind': ['error', '/^v-/'],
-    'vue/no-useless-v-bind': 'error',
-    'vue/no-unused-refs': 'error',
-    'vue/padding-line-between-blocks': ['error', 'always'],
-    'vue/prefer-separate-static-class': 'error',
-
-    // extensions
-    'vue/array-bracket-spacing': ['error', 'never'],
-    'vue/arrow-spacing': ['error', { before: true, after: true }],
-    'vue/block-spacing': ['error', 'always'],
-    'vue/brace-style': ['error', '1tbs', { allowSingleLine: true }],
-    'vue/comma-dangle': ['error', 'always-multiline'],
-    'vue/comma-spacing': ['error', { before: false, after: true }],
-    'vue/comma-style': ['error', 'last'],
-    'vue/dot-location': ['error', 'property'],
-    'vue/dot-notation': ['error', { allowKeywords: true }],
-    'vue/eqeqeq': ['error', 'smart'],
-    // 'vue/func-call-spacing': ['off', 'never'],
-    'vue/key-spacing': ['error', { beforeColon: false, afterColon: true }],
-    'vue/keyword-spacing': ['error', { before: true, after: true }],
-    'vue/no-constant-condition': 'warn',
-    'vue/no-empty-pattern': 'error',
-    'vue/no-extra-parens': ['error', 'functions'],
-    'vue/no-irregular-whitespace': 'error',
-    'vue/no-loss-of-precision': 'error',
-    'vue/no-restricted-syntax': ['error', 'DebuggerStatement', 'LabeledStatement', 'WithStatement'],
-    'vue/no-sparse-arrays': 'error',
-    'vue/object-curly-newline': ['error', { multiline: true, consistent: true }],
-    'vue/object-curly-spacing': ['error', 'always'],
-    'vue/object-property-newline': ['error', { allowMultiplePropertiesPerLine: true }],
-    'vue/object-shorthand': [
-      'error',
-      'always',
-      {
-        ignoreConstructors: false,
-        avoidQuotes: true
-      }
-    ],
-    'vue/operator-linebreak': ['error', 'before'],
-    'vue/prefer-template': 'error',
-    'vue/quote-props': ['error', 'consistent-as-needed'],
-    'vue/space-in-parens': ['error', 'never'],
-    'vue/space-infix-ops': 'error',
-    'vue/space-unary-ops': ['error', { words: true, nonwords: false }],
-    'vue/template-curly-spacing': 'error'
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "never",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }]
   }
 }
