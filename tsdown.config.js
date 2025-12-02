3584df80d403631030b769ed5bc65ca6a71185a8
脆皮丶 <cuiqg@foxmail.com>
2025-12-02 23:16:36 +0800

    chore: 优化构建配置并添加 package.json 导出

--- tsdown.config.js
+++ tsdown.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'tsdown'
-import { spawn } from 'node:child_process'
-import process from 'node:process'
+
 export default defineConfig({
   entry: 'src/index.js',
   clean: true,
   shims: true,
   format: ['esm'],
-  exports: true,
-  hooks(hooks) {
-    hooks.hook('build:done', () => {
-      console.log(process.env.NODE_ENV)
-      // 运行 eslint-config-inspector
-      spawn('npx', ['eslint-config-inspector', '--open', 'false', '--config', 'eslint-inspector.config.js'], {
-        stdio: 'inherit',
-        shell: true
-      })
-    })
-  }
+  exports: true
 })
