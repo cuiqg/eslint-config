abf11679005b66a6fcab5d23c6ce22b9464b4383
脆皮丶 <cuiqg@foxmail.com>
2025-12-03 23:44:57 +0800

    feat: 新增 tailwindcss 配置并将 npm 命令替换为 pnpm

---
+++ src/configs/tailwindcss.js
@@ -0,0 +1,25 @@
+import { interopDefault } from '../utils'
+
+export async function tailwindcss() {
+  const pluginTailwindcss = await interopDefault(import('eslint-plugin-tailwindcss'))
+
+  return [
+    {
+      name: 'cuiqg/tailwindcss',
+      plugins: {
+        tailwindcss: pluginTailwindcss
+      },
+      languageOptions: {
+        parserOptions: {
+          ecmaFeatures: {
+            jsx: true,
+          },
+        },
+      },
+      rules: {
+        ...pluginTailwindcss.configs['flat/recommended'][1].rules,
+      }
+
+    }
+  ]
+}
